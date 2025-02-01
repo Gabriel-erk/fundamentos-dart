eeb6e7897bb7715b0aa870dc1bc9267ff718b046
Gabriel Erick <gabrielmmve13@gmail.com>
2025-01-31 21:59:42 -0300

    implementando tela principal

--- fundamentos/lib/cadastro_usuario_flutter/lib/view/tela_principal.dart
+++ fundamentos/lib/cadastro_usuario_flutter/lib/view/tela_principal.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:cadastro_usuario_flutter/view/usuario_view.dart';
 
 class TelaPrincipal extends StatelessWidget {
-  const TelaPrincipal({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sistema de Cadastro'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text(
+                'Menu Principal',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Cadastro de Usuários'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => UsuarioView()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('Sair'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text(
+          'Sistema de cadastro de usuário',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
     );
   }
 }
